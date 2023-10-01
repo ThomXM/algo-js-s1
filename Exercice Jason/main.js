cf62f9030934e9d4ffb24b612da64713792a9391
ThomasCourtois <146062279+ThomXM@users.noreply.github.com>
2023-10-01 20:44:48 +0200

    Update main.js

---
+++ Exercice Jason/main.js
@@ -0,0 +1,59 @@
+@@ -0,0 +1,58 @@
+// Classe Jason //
+
+class tueur {
+    constructor(name, santé) {
+        this.name = name
+        this.santé = santé
+
+    }
+}
+
+let Jason = new tueur ('Jason ', 100 )
+
+// Listes : Caractéristiques + Noms //
+
+let caractéristiques = ["nerd", "sportif", "blonde", , "ancien militaire", "medecin"]
+
+let noms = ['Steve', 'Kate', 'Quentin', 'William', 'Ada']
+
+// classe survivants //
+
+
+
+//  Creation des survivants //
+
+class survivants {
+    constructor(name, caractéristique) {
+        this.name = name
+        this.caractéristique = caractéristique
+
+    }
+}
+
+// -- Pour cette partie je voulais utiliser la méthode Random pour attribuer un NOM et une CARACTERISTIQUE -- //
+// -- à chacun des survivants -- //
+
+
+let survivant1 = ""
+let survivant2 = "" 
+let survivant3 = "" 
+let survivant4 = "" 
+let survivant5 = "" 
+
+//
+
+
+
+
+// Conditions de sorties //
+
+
+if (survivant1, survivant2, survivant3, survivant4, survivant5 ){
+    console.log('Jason à gagné, tout les survivants sont mort !')
+
+}
+
+if (Jason.santé < 1) {
+    console.log("Jason est mort, les survivants ont gagné !")
+}
